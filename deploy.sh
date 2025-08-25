d70fd28c08cfcfff34a015c195857ce859d35337
Mr_Dwj <explorer-dong@foxmail.com>
2025-08-26 00:59:26 +0800

    feat: migrate web to oss

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,6 @@
-echo "部署到 GitHub Pages..."
 git push
 
-
-echo "部署到 Aliyun Server..."
 hexo clean
 hexo generate
-hexo deploy
-ossutil cp e:/_notes/Blog/public/local-search.xml oss://dwj-oss/files/ --force
+ossutil rm -r oss://blog-web-shanghai/ -f
+ossutil cp -r ./public/ oss://blog-web-shanghai/
