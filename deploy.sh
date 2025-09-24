cd4db5d6647076c9b4de607287dd1a7d30643938
Mr_Dwj <explorer-dong@foxmail.com>
2025-09-24 12:09:54 +0800

    add: traffic savior & upd: web arch

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,29 @@
+# ============================================================
+echo "备份到 GitHub"
 git push
 
+# ============================================================
+echo "生成静态页面"
 hexo clean
 hexo generate
-ossutil rm -r oss://blog-web-shanghai/ -f
-ossutil cp -r ./public/ oss://blog-web-shanghai/
+
+# ============================================================
+echo "部署到 Server"
+# 本地压缩传输
+tar -czf blog.tar.gz public/
+sftp git@47.100.217.241 << EOF
+put -r blog.tar.gz /home/web/
+bye
+EOF
+rm blog.tar.gz
+
+# 云端解压配置
+ssh git@47.100.217.241 << EOF
+cd /home/web/
+rm -rf blog/
+mkdir blog/
+chown -R git:git blog/
+tar -xzf blog.tar.gz -C blog/ --strip-components=1
+rm blog.tar.gz
+exit
+EOF
