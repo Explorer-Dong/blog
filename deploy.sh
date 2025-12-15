74bc8f00092de5198c63daf61dca545a3fcd666e
Mr_Dwj <explorer-dong@foxmail.com>
2025-12-15 10:39:03 +0800

    feat: support github actions

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-# backup to GitHub
-git push
-
-# deploy to Aliyun OSS
-hexo clean
-hexo generate
-ossutil rm -r oss://blog-web-shanghai/ -f
-ossutil cp -r ./public/ oss://blog-web-shanghai/
-hexo clean
